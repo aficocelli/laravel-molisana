003148478dfbebf0476e4c6730aa8b8037c036cd
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-07 16:26:31 +0200

    add. nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>La Molisana</title>
+        {{-- google font --}}
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+        {{-- /google font --}}
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
         {{-- header --}}
         @include('parts.header')
-        {{-- header --}}
+        {{-- /header --}}
+        {{-- nav --}}
+        @include('parts.nav')
+        {{-- /nav --}}
     </body>
 </html>
---
+++ resources/views/parts/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="main-menu">
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Prodotti</a></li>
+        <li><a href="">News</a></li>
+    </ul>
+</nav>
