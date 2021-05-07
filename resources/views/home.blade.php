01882e892b3d2ec8017f916fea4cc772279929d0
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-07 17:42:32 +0200

    add. main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,52 @@
         {{-- header --}}
         @include('parts.header')
         {{-- /header --}}
+
         {{-- nav --}}
         @include('parts.nav')
         {{-- /nav --}}
+
+        {{-- main --}}
+        <main>
+            {{-- section pasta lunga --}}
+            <section class="product-section">
+                <h2>Le lunghe</h2>
+                <div class="container">
+                    @foreach ($lunga as $pasta)
+                    <div class="box">
+                        <img src="{{$pasta['src']}}" alt="">
+                    </div>
+                    @endforeach
+                </div>
+            </section>
+            {{-- /section pasta lunga --}}
+
+            {{-- section pasta corta --}}
+            <section class="product-section">
+                <h2>Le corte</h2>
+                <div class="container">
+                    @foreach ($corta as $pasta)
+                    <div class="box">
+                        <img src="{{$pasta['src']}}" alt="">
+                    </div>
+                    @endforeach
+                </div>
+            </section>
+            {{-- /section pasta corta --}}
+
+            {{-- section pasta cortissima --}}
+            <section class="product-section">
+                <h2>Le cortissime</h2>
+                <div class="container">
+                    @foreach ($cortissima as $pasta)
+                    <div class="box">
+                        <img src="{{$pasta['src']}}" alt="">
+                    </div>
+                    @endforeach
+                </div>
+            </section>
+            {{-- /section pasta corta --}}
+        </main>
+        {{-- /main --}}
     </body>
 </html>
