159118e46d6734181e4b4763110049d69dafa6a5
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-10 14:41:34 +0200

    fix. footer

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -10,27 +10,44 @@
                 <li><a href="">Pec: lamolisana@pec.it</a></li>
                 <li><a href="">Tel: +39 0874 4981</a></li>
                 <li><a href="">Fax: +39 0874 629584</a></li>
+                <li><a href="">info@lamolisana.it(per segnalazione degli utenti)</a></li>
+                <li><a href="">commerciale@lamolisana.it</a></li>
+                <li><a href="">export@lamolisana.it</a></li>
+                <li><a href="">numero verde 800629584</a></li>
+                <li><a href="">telefono 380 629584</a></li>
                 
             </ul>
         </div>
-        <div class="foot-box">
+        <div class="foot-box footer-list">
             <ul>
-                <li><a href="">Ragione sociale: La Molisana S.P.A.</a></li>
-                <li><a href="">Sede legale: Contrada Delle Alpi , 100/A 86100 - Campobasso</a></li>
-                <li><a href="">Pec: lamolisana@pec.it</a></li>
-                <li><a href="">Tel: +39 0874 4981</a></li>
-                <li><a href="">Fax: +39 0874 629584</a></li>
-                
+                <li>PASTIFICIO</li>
+                <li><a href="">Il pastificio</a></li>
+                <li><a href="">Gran decorticato a pietra</a></li>
+                <li><a href="">Il molise c'è</a></li>
+                <li><a href="">Filiera integrata</a></li>
+                <li><a href="">100 anni di pasta</a></li>
+                <li><a href="">Sartoria della pasta</a></li>
+                <li><a href="">Spaghetto Quadrato</a></li>
+                <li><a href="">Le Persone</a></li>            
+            </ul>
+            <ul>
+                <li>PRODOTTI</li>
+                <li><a href="">Le Classiche</a></li>
+                <li><a href="">Le Integrali</a></li>
+                <li><a href="">Le Speciali</a></li>
+                <li><a href="">Le Biologiche</a></li>
+                <li><a href="">Le Gluten-Free</a></li>
+                <li><a href="">Le Semole</a></li>
+                <li><a href="">Le Extra di Lusso</a></li>           
             </ul>
         </div>
-        <div class="foot-box">
+        <div class="foot-box footer-list">
             <ul>
-                <li><a href="">Ragione sociale: La Molisana S.P.A.</a></li>
-                <li><a href="">Sede legale: Contrada Delle Alpi , 100/A 86100 - Campobasso</a></li>
-                <li><a href="">Pec: lamolisana@pec.it</a></li>
-                <li><a href="">Tel: +39 0874 4981</a></li>
-                <li><a href="">Fax: +39 0874 629584</a></li>
-                
+                <li>COLLEZIONE DA CHEF</li>
+                <li><a href="">Collezione da Chef</a></li>
+                <li><a href="">Grandi Cucine</a></li>
+                <li><a href="">Biologiche</a></li>
+                <li><a href="">Quadrate</a></li>
             </ul>
         </div>
     </div>
